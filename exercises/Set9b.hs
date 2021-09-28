150f68b00019ce78fba4d7b0fe24b34c07c7692b
ofthegoats <47647338+ofthegoats@users.noreply.github.com>
2021-09-28 21:13:27 +0100

    Completed Set9b

--- exercises/Set9b.hs
+++ exercises/Set9b.hs
@@ -1,8 +1,9 @@
+{-# OPTIONS_GHC -Wno-incomplete-patterns #-}
+
 module Set9b where
 
-import Mooc.Todo
-
 import Data.List
+import Mooc.Todo
 
 --------------------------------------------------------------------------------
 -- Ex 1: In this exercise set, we'll solve the N Queens problem step by step.
@@ -42,15 +43,17 @@ import Data.List
 -- the roles of different function arguments clearer without adding syntactical
 -- overhead:
 
-type Row   = Int
-type Col   = Int
+type Row = Int
+
+type Col = Int
+
 type Coord = (Row, Col)
 
 nextRow :: Coord -> Coord
-nextRow (i,j) = todo
+nextRow (i, j) = (i + 1, 1)
 
 nextCol :: Coord -> Coord
-nextCol (i,j) = todo
+nextCol (i, j) = (i, j + 1)
 
 --------------------------------------------------------------------------------
 -- Ex 2: Implement the function prettyPrint that, given the size of
@@ -100,7 +103,17 @@ nextCol (i,j) = todo
 -- takes O(n^3) time. Just ignore the previous sentence, if you're not familiar
 -- with the O-notation.)
 prettyPrint :: Size -> [Coord] -> String
-prettyPrint = todo
+prettyPrint n coords = prettyPrint' n (sortBy (\(a, b) (c, d) -> if a < c || (a == c && b < d) then LT else GT) coords) (1, 1)
+  where
+    prettyPrint' n [] (y, x)
+      | x > n = '\n' : prettyPrint' n [] (y + 1, 1)
+      | y > n = []
+      | otherwise = '.' : prettyPrint' n [] (y, x + 1)
+    prettyPrint' n qs (y, x)
+      | (y, x) == head qs = 'Q' : prettyPrint' n (tail qs) (y, x + 1)
+      | x > n = '\n' : prettyPrint' n qs (y + 1, 1)
+      | y > n = []
+      | otherwise = '.' : prettyPrint' n qs (y, x + 1)
 
 --------------------------------------------------------------------------------
 -- Ex 3: The task in this exercise is to define the relations sameRow, sameCol,
@@ -124,16 +137,19 @@ prettyPrint = todo
 --   sameAntidiag (500,5) (5,500) ==> True
 
 sameRow :: Coord -> Coord -> Bool
-sameRow (i,j) (k,l) = todo
+sameRow (i, j) (k, l) = i == k
 
 sameCol :: Coord -> Coord -> Bool
-sameCol (i,j) (k,l) = todo
+sameCol (i, j) (k, l) = j == l
+
+-- by considering Pythagoras, these two tests turn out the exact same
+-- the triangle has a right angle and the two others are pi/4, which is how we arrive at this
 
 sameDiag :: Coord -> Coord -> Bool
-sameDiag (i,j) (k,l) = todo
+sameDiag (i, j) (k, l) = (i - k) ^ 2 == (j - l) ^ 2
 
 sameAntidiag :: Coord -> Coord -> Bool
-sameAntidiag (i,j) (k,l) = todo
+sameAntidiag (i, j) (k, l) = (i - k) ^ 2 == (j - l) ^ 2
 
 --------------------------------------------------------------------------------
 -- Ex 4: In chess, a queen may capture another piece in the same row, column,
@@ -184,12 +200,14 @@ sameAntidiag (i,j) (k,l) = todo
 -- First Out (LIFO) manner, so we give this type the alias Stack:
 -- https://en.wikipedia.org/wiki/Stack_(abstract_data_type)
 
-type Size      = Int
+type Size = Int
+
 type Candidate = Coord
-type Stack     = [Coord]
+
+type Stack = [Coord]
 
 danger :: Candidate -> Stack -> Bool
-danger = todo
+danger cand = any (\q -> sameRow cand q || sameCol cand q || sameDiag cand q)
 
 --------------------------------------------------------------------------------
 -- Ex 5: In this exercise, the task is to write a modified version of
@@ -224,7 +242,14 @@ danger = todo
 -- solution to this version. Any working solution is okay in this exercise.)
 
 prettyPrint2 :: Size -> Stack -> String
-prettyPrint2 = todo
+prettyPrint2 n qs = prettyPrint2' n qs (1, 1)
+  where
+    prettyPrint2' n qs (y, x)
+      | x > n = '\n' : prettyPrint2' n qs (y + 1, 1)
+      | y > n = []
+      | (y, x) `elem` qs = 'Q' : prettyPrint2' n qs (y, x + 1)
+      | (y, x) `danger` qs = '#' : prettyPrint2' n qs (y, x + 1)
+      | otherwise = '.' : prettyPrint2' n qs (y, x + 1)
 
 --------------------------------------------------------------------------------
 -- Ex 6: Now that we can check if a piece can be safely placed into a square in
@@ -269,17 +294,24 @@ prettyPrint2 = todo
 --     Q#######
 
 fixFirst :: Size -> Stack -> Maybe Stack
-fixFirst n s = todo
+fixFirst n ((i, j) : qs)
+  | i > n || j > n = Nothing
+  | not . danger (i, j) $ qs = Just $ (i, j) : qs
+  | otherwise = fixFirst n ((i, j + 1) : qs)
 
 --------------------------------------------------------------------------------
 -- Ex 7: We need two helper functions for stack management.
 --
+
 -- * continue moves on to a new row. It pushes a new candidate to the
+
 --   top of the stack (front of the list). The new candidate should be
 --   at the beginning of the next row with respect to the queen
 --   previously on top of the stack.
 --
+
 -- * backtrack moves back to the previous row. It removes the top
+
 --   element of the stack, and adjusts the new top element so that it
 --   is in the next column.
 --
@@ -291,10 +323,10 @@ fixFirst n s = todo
 -- Hint: Remember nextRow and nextCol? Use them!
 
 continue :: Stack -> Stack
-continue s = todo
+continue (q : qs) = nextRow q : q : qs
 
 backtrack :: Stack -> Stack
-backtrack s = todo
+backtrack (x : y : qs) = nextCol y : qs
 
 --------------------------------------------------------------------------------
 -- Ex 8: Let's take a step. Our algorithm solves the problem (in a
@@ -363,7 +395,9 @@ backtrack s = todo
 --     step 8 [(6,1),(5,4),(4,2),(3,5),(2,3),(1,1)] ==> [(5,5),(4,2),(3,5),(2,3),(1,1)]
 
 step :: Size -> Stack -> Stack
-step = todo
+step n qs = case fixFirst n qs of
+  Nothing -> backtrack qs
+  Just (x : xs) -> nextRow (head qs) : x : xs
 
 --------------------------------------------------------------------------------
 -- Ex 9: Let's solve our puzzle! The function finish takes a partial
@@ -378,7 +412,9 @@ step = todo
 -- solve the n queens problem.
 
 finish :: Size -> Stack -> Stack
-finish = todo
+finish n qs
+  | length qs == n + 1 = tail qs
+  | otherwise = finish n $ step n qs
 
 solve :: Size -> Stack
-solve n = finish n [(1,1)]
+solve n = finish n [(1, 1)]
